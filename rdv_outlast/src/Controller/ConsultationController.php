a9481eb830ed23a52a9cb32dcc456cc428d34a9b
Jogozan <bouguerouche593@gmail.com>
2021-11-16 14:58:25 +0100

    afficher les patients tout court

--- rdv_outlast/src/Controller/ConsultationController.php
+++ rdv_outlast/src/Controller/ConsultationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\RDV;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class ConsultationController extends AbstractController
     #[Route('/consultation', name: 'consultation')]
     public function index(): Response
     {
-        return $this->render('consultation/index.html.twig', [
-            'controller_name' => 'ConsultationController',
-        ]);
+
+        $repository=$this->getDoctrine()->getRepository(RDV::class);
+		$lesRdv=$repository->findAll();
+		return $this->render('consultation/lstAvion.html.twig',['rdv' => $lesRdv,]);
+        
     }
 }
